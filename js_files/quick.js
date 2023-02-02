f919e8e12bb7f6fedf8ee6f6bc960139d6bb6ecf
Alok Yadav <alokyadav0912@gmail.com>
2023-02-02 12:31:25 +0530

    quick sort

--- js_files/quick.js
+++ js_files/quick.js
@@ -7,44 +7,60 @@ function swap(el1,el2) {
     el2.style.height = transform1;
     } ;
 
+function partition(low,high){
+    arr[high].style.background = "red" ;
+    let pivot = parseFloat(arr[high].style.height); 
+    let i = (low - 1);
+    for (let j = low; j <= high - 1; j++)
+    {
+        if (parseFloat(arr[j].style.height) < pivot)
+        {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    arr[high].style.background = "rgba(231, 183, 78, 0.856)" ;
+    swap(arr[i + 1], arr[high]);
+    arr[i+1].style.background = "#198754" ;
+    return i + 1;
 
-async function selectionSort() {
+} ;
+
+async function quickUtil(low,high)
+{
+    if (low < high)
+    {
+        
+        let pi = partition(low, high);
+        await new Promise(resolve => setTimeout(() => {resolve();},500));  
+        
+        await quickUtil(low, pi - 1);
+        await quickUtil(pi + 1, high);
+    }
+
+} ;
+
+async function quickSort() {
     let i ;
-    algo.innerHTML = "SELECTION SORT" ;
+    algo.innerHTML = "QUICK SORT" ;
     disableFunc() ;
     for(i=0;i<arr.length;i++)
     {
         swap(arr[i],arr[i]);
     }
-    let j ;
-    let min_idx ;
-    for (i = 0; i < arr.length-1; i++)
+    
+    await quickUtil(0,arr.length-1) ;
+    for(i=0;i<arr.length;i++)
     {
-        min_idx = i;
-        for (j = i+1; j <arr.length; j++)
-        {
-            if (parseFloat(arr[j].style.height) < parseFloat(arr[min_idx].style.height))
-                min_idx = j;
-
-        // arr[i].style.background = "red" ;
-        // arr[min_idx].style.background = "red" ;
-        
-        }
-        arr[min_idx].style.background = "#dc3545" ;
-        arr[i].style.background = "#dc3545" ;
-        swap(arr[min_idx], arr[i]);
-        await new Promise(resolve => setTimeout(() => {resolve();},100));
-        arr[min_idx].style.background = "rgba(231, 183, 78, 0.856)" ;
         arr[i].style.background = "#198754" ;
     }
-    arr[i].style.background = "#198754" ;
-    enableFunc() ;
+    enableFunc();
 } ;
 
-let selection = document.getElementsByClassName("selection-sort") ;
+let quick = document.getElementsByClassName("quick-sort") ;
 
 
 
-selection[0].addEventListener('click',()=>{
-    selectionSort() ;
+quick[0].addEventListener('click',()=>{
+     quickSort() ;
 });
